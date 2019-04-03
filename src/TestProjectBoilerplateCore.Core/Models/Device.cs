84c44479a15e44963920a37aa1eddbfe1aa46876
Bane1984 <anajole_8586@outlook.com>
2019-04-03 15:54:47 +0200

    Add project files.

---
+++ src/TestProjectBoilerplateCore.Core/Models/Device.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Abp.Domain.Entities;
+
+namespace TestProjectBoilerplateCore.Models
+{
+    public class Device:Entity
+    {
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public int DeviceTypeId { get; set; }
+        [ForeignKey("DeviceTypeId")]
+        public DeviceType DeviceType { get; set; }
+
+        public List<DeviceTypePropertie> DeviceTypeProperties { get; set; } = new List<DeviceTypePropertie>();
+        public List<DevicePropertyValue> DevicePropertyValueses { get; set; } = new List<DevicePropertyValue>();
+    }
+}
---
+++ src/TestProjectBoilerplateCore.Core/Models/DeviceTypePropertie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Abp.Domain.Entities;
+using Remotion.Linq.Parsing.Structure.NodeTypeProviders;
+
+namespace TestProjectBoilerplateCore.Models
+{
+    public class DeviceTypePropertie:Entity
+    {
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public int DeviceTypeId { get; set; }
+        [ForeignKey("DeviceTypeId")]
+        public DeviceType DeviceType { get; set; }
+
+        public bool isRequired { get; set; }
+    }
+}
